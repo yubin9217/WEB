317c787aea37b01ea75897fa9f35db33391dc5ba
yubin9217 <yubin9217@qq.com>
2017-09-07 18:05:05 +0800

    更改nodejs00文件夹名后，重新调整代码中对module的引用代码

--- serverside/nodejs测试代码/testClosure.js
+++ serverside/nodejs测试代码/testClosure.js
@@ -1,7 +1,7 @@
-var testClosure=require("./moduleClosure.js");
+var testClosure=require("./nodeModuleClosure.js");
 var array=[];
 function a(i){
-	console.log(i+"异步方法执行完闭!");
+    console.log(i+"异步方法执行完闭!");
 }
 testClosure(array,a);
 console.log("主程序执行完毕");
